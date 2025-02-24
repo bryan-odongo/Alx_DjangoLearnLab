82fbc64fd7bf3054d3c1804b87450cda370afd09
Bryan Odongo <152975669+bryan-odongo@users.noreply.github.com>
2025-02-24 06:22:47 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,42 @@
 from django.urls import path
-from .views import add_book, edit_book, delete_book, register, user_login, user_logout
-from django.contrib.auth import views as auth_views
+from .views import (
+    list_books, 
+    LibraryDetailView, 
+    add_book, 
+    edit_book, 
+    delete_book, 
+    register, 
+    user_login, 
+    user_logout
+)
 
 urlpatterns = [
-    
-    path("books/add/", add_book, name="add_book"),
-    path("books/edit/<int:book_id>/", edit_book, name="edit_book"),
-    path("books/delete/<int:book_id>/", delete_book, name="delete_book"),
-
     path("register/", register, name="register"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
+    path("books/add/", add_book, name="add_book"),
+    path("books/edit/<int:book_id>/", edit_book, name="edit_book"),
+    path("books/delete/<int:book_id>/", delete_book, name="delete_book"),
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
+
+
+
+
+
+# from django.urls import path
+# from .views import add_book, edit_book, delete_book, register, user_login, user_logout
+# from django.contrib.auth import views as auth_views
+
+# urlpatterns = [
+    
+#     path("books/add/", add_book, name="add_book"),
+#     path("books/edit/<int:book_id>/", edit_book, name="edit_book"),
+#     path("books/delete/<int:book_id>/", delete_book, name="delete_book"),
+
+#     path("register/", register, name="register"),
+#     path("login/", user_login, name="login"),
+#     path("logout/", user_logout, name="logout"),
+# ]
 
