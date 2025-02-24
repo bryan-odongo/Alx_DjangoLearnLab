5866207649f7afeffffa22a923f29a08b2e4c4a1
Bryan Odongo <152975669+bryan-odongo@users.noreply.github.com>
2025-02-24 06:20:46 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  # Required import for DetailView
 from .models import Book, Library
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
