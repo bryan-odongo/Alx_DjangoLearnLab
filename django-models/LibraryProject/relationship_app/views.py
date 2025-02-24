b60be80968c95e37fd8d02dca8d3642b8c1f26d0
Bryan Odongo <saonarawestinc@outlook.com>
2025-02-23 18:36:33 -0800

    Updated views v1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Book, Library
 # Function-Based View: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/pist_books.html', {'books': books})
 
 # Class-Based View: Display library details
 class LibraryDetailView(DetailView):
