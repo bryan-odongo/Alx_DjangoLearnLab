13bc0b162d3c143adc5c9621be8c70159b8a61ab
Bryan Odongo <152975669+bryan-odongo@users.noreply.github.com>
2025-03-02 10:05:32 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    # path('admin/', views.admin_view, name='admin_view'),
+    
     path('admin/', views.admin_view, name='admin_view'),
+
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),
