3ab4fbdfa177684faa4f28b8fe128c78a737745c
Bryan Odongo <152975669+bryan-odongo@users.noreply.github.com>
2025-03-02 10:46:14 +0300

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/LibraryProject/urls.py
+++
@@ -1,9 +0,0 @@
-# project/urls.py
-
-from django.contrib import admin
-from django.urls import path, include  # Make sure to include 'include' here
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Existing URL for the admin interface   
-    path('relationship_app/', include('relationship_app.urls')),  # Add this line
-]
